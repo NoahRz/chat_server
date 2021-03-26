22c9ff82b31531d4df3543609c28af851c9d0edc
Noah Razafindrabe <noah.razafindrabe@outlook.com>
2021-03-26 20:35:00 +0100

    connected to mongo and redis

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var i;
 
+//Use chalk to add colours on the console
+var chalk = require('chalk');
+
 /**
  * Gestion des requêtes HTTP des utilisateurs en leur renvoyant les fichiers du dossier 'public'
  */
@@ -23,6 +26,19 @@ var messages = [];
  * Liste des utilisateurs en train de saisir un message
  */
 var typingUsers = [];
+
+// set up mongo
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27020/";
+
+//redis
+const redis = require("redis");
+const redisClient = redis.createClient();
+
+redisClient.on("error", function (error) {
+  console.error(error);
+});
+
 
 io.on('connection', function (socket) {
 
@@ -65,6 +81,10 @@ io.on('connection', function (socket) {
       if (userIndex !== -1) {
         users.splice(userIndex, 1);
       }
+
+      // Suppresion de l'utilisateur de Redis
+      removeUserConnectedFromRedis(loggedUser);
+
       // Ajout du message à l'historique
       messages.push(serviceMessage);
       // Emission d'un 'user-logout' contenant le user
@@ -92,6 +112,10 @@ io.on('connection', function (socket) {
       // Sauvegarde de l'utilisateur et ajout à la liste des connectés
       loggedUser = user;
       users.push(loggedUser);
+
+      // sauvegarde du user connecte dans redis
+      storeUserConnectedToRedis(loggedUser);
+
       // Envoi et sauvegarde des messages de service
       var userServiceMessage = {
         text: 'You logged in as "' + loggedUser.username + '"',
@@ -118,6 +142,10 @@ io.on('connection', function (socket) {
   socket.on('chat-message', function (message) {
     // On ajoute le username au message et on émet l'événement
     message.username = loggedUser.username;
+
+    // stocke le message dans mongodb
+    storeMsgToMongo(message);
+
     io.emit('chat-message', message);
     // Sauvegarde du message
     messages.push(message);
@@ -157,3 +185,23 @@ io.on('connection', function (socket) {
 http.listen(3000, function () {
   console.log('Server is listening on *:3000');
 });
+
+function storeMsgToMongo(message) {
+  MongoClient.connect(url, function (err, client) {
+    if (err) throw err;
+    var db = client.db("chat_server");
+    db.collection("messages").insertOne(message, function (err, res) {
+      if (err) throw err;
+      console.log("1 document inserted");
+      client.close();
+    });
+  });
+};
+
+function storeUserConnectedToRedis(loggedUser) {
+  redisClient.sadd("loggedUsers", loggedUser.username);
+};
+
+function removeUserConnectedFromRedis(loggedUser) {
+  redisClient.srem("loggedUsers", loggedUser.username);
+};
